b9960b4d597518b44d20383fdee292a13ae7a8c4
LAPTOP-NB0G3E1B\sheep <mori.shotaro.84w@st.kyoto-u.ac.jp>
2022-08-06 17:26:23 +0900

    add basic plot scripts

---
+++ prog/function.R
@@ -0,0 +1,162 @@
+fplot_area <- function(var, totvar=NA, ylabel, w=150, h=100, cpt=NULL, name_file, adj_position='stack'){
+  dt <- filter(df_all, Variable %in% var$Variable) %>%
+    mutate(Scenario=factor(Scenario, levels=scen_mat$Scenario)) %>%
+    mutate(cat1=scen_mat$scen_cpol[Scenario], cat2=scen_mat$scen_techpol[Scenario]) %>%
+    mutate(cat1=factor(cat1, levels=unique(scen_mat$scen_cpol))) %>%
+    mutate(cat2=factor(cat2, levels=unique(scen_mat$scen_techpol))) %>%
+    mutate(Variable=factor(Variable, levels=rev(var$Variable))) %>%
+    filter(Scenario!='historical')
+  dt_tot <- filter(df_all, Variable==totvar) %>%
+    mutate(Scenario=factor(Scenario, levels=scen_mat$Scenario)) %>%
+    mutate(cat1=scen_mat$scen_cpol[Scenario], cat2=scen_mat$scen_techpol[Scenario]) %>%
+    mutate(cat1=factor(cat1, levels=unique(scen_mat$scen_cpol))) %>%
+    mutate(cat2=factor(cat2, levels=unique(scen_mat$scen_techpol))) %>%
+    filter(Scenario!='historical')
+  plt$color <- as.character(var$Color); names(plt$color) <- as.character(var$Variable)
+  plt$legend <- as.character(var$Legend); names(plt$legend) <- as.character(var$Variable)
+  if(nrow(dt)>0){
+    p <- ggplot(data=dt)+
+      geom_area(aes(x=Year, y=Value, fill=Variable), position=adj_position, show.legend=T, alpha=1)+
+      geom_path(data=dt_tot, aes(x=Year, y=Value), show.legend=F, alpha=1)+
+      scale_fill_manual(values=plt$color, labels=plt$legend, name="Variable")
+    if(all(is.na(scen_mat$scen_techpol))){
+      p <- p + facet_grid(.~cat1)
+    } else {
+      p <- p + facet_grid(cat2~cat1)}
+    p <- p+labs(title=cpt, x=NULL, y=ylabel)+
+      theme_bw()+
+      theme(legend.position='bottom', legend.title=element_blank(), 
+            axis.text.x=element_text(angle=45, hjust=1),
+            strip.background=element_blank(),
+            panel.grid.minor=element_blank())
+    if(name_file!=F){
+      ggsave(filename=paste0(odir,"/general/",name_file,".png"), plot=p, width=w, height=h, units='mm', dpi=300)
+    }
+    return(p)
+  } else {
+    print('error: empty data frame')
+  }
+}
+
+fplot_bar <- function(var, per=year_all, ylabel, w=150, h=100, cpt=NULL, name_file){
+  dt <- filter(df_all, Variable%in%var, Year%in%per) %>%
+    filter(!(Year==2010 & Scenario%in%scen_all[-1])) %>%
+    mutate(Scenario=factor(Scenario, levels=scen_mat$Scenario)) %>%
+    mutate(Variable=factor(Variable, levels=rev(var$Variable))) %>%
+    filter(Scenario!='historical')
+  plt$color <- as.character(var$Color); names(plt$color) <- as.character(var$Variable)
+  plt$legend <- as.character(var$Legend); names(plt$legend) <- as.character(var$Variable)
+  if(nrow(dt)>0){
+    p <- ggplot(data=dt)+
+      geom_bar(aes(x=Scenario, y=Value, fill=Variable), stat='identity', position='stack', show.legend=T, alpha=1)+
+      scale_fill_manual(values=plt$color, labels=plt$legend, name='Variable')+
+      facet_grid(.~Year, scales='free_x', space='free_x')+
+      labs(title=cpt, x=NULL, y=ylabel)+
+      theme_bw()+
+      theme(legend.position='right', strip.background=element_blank(),panel.grid.minor=element_blank())
+    if(name_file!=F){
+      ggsave(filename=paste0(odir,"/plot/",reg_in,"/", name_file, ".png"), plot=p, width=w, height=h, units='mm', dpi=300)
+    }
+    return(p)
+  } else {
+    print('error: empty data frame')
+  }
+}
+
+fplot_fbar <- function(var, ylabel, w=150, h=100, cpt=NULL, name_file, adj_position='fill'){
+  dt <- filter(df_all, Variable %in% var$Variable) %>%
+    mutate(Scenario=factor(Scenario, levels=scen_mat$Scenario)) %>%
+    mutate(cat1=scen_mat$scen_cpol[Scenario], cat2=scen_mat$scen_techpol[Scenario]) %>%
+    mutate(cat1=factor(cat1, levels=unique(scen_mat$scen_cpol))) %>%
+    mutate(cat2=factor(cat2, levels=unique(scen_mat$scen_techpol))) %>%
+    mutate(Variable=factor(Variable, levels=rev(var$Variable))) %>%
+    filter(Scenario!='historical')
+  plt$color <- as.character(var$Color); names(plt$color) <- as.character(var$Variable)
+  plt$legend <- as.character(var$Legend); names(plt$legend) <- as.character(var$Variable)
+  if(nrow(dt)>0){
+    p <- ggplot(data=dt)+
+      geom_bar(aes(x=Year, y=Value, fill=Variable), stat="identity", position=adj_position, show.legend=T, alpha=1)+
+      scale_fill_manual(values=plt$color, labels=plt$legend, name="Variable")+
+      scale_y_continuous(labels=percent)
+    if(all(is.na(scen_mat$scen_techpol))){
+      p <- p + facet_grid(.~cat1)
+    } else {
+      p <- p + facet_grid(cat2~cat1)}
+    p <- p+labs(title=cpt, x=NULL, y=ylabel)+
+      theme_bw()+
+      theme(legend.position='bottom', legend.title=element_blank(), 
+            axis.text.x=element_text(angle=45, hjust=1),
+            strip.background=element_blank(),
+            panel.grid.minor=element_blank())
+    if(name_file!=F){
+      ggsave(filename=paste0(odir,"/general/",name_file,".png"), plot=p, width=w, height=h, units='mm', dpi=300)
+    }
+    return(p)
+  } else {
+    print('error: empty data frame')
+  }
+}
+
+fplot_path <- function(var, ylabel, ylim=NULL, ylab=waiver(), w=150, h=100, cpt=NULL, name_file){
+  dt <- filter(df_all, Variable %in% var) %>%
+    mutate(Scenario=factor(Scenario, levels=scen_mat$Scenario)) %>%
+    filter(Scenario!='historical')
+  if(nrow(dt)>0){
+    p <- ggplot(data=dt)+
+      geom_path(aes(x=Year, y=Value, color=Scenario), show.legend=T, alpha=1)+
+      labs(title=cpt, x=NULL, y=ylabel)+
+      scale_y_continuous(limits=ylim, labels=ylab)
+    if(length(var) > 1){
+      p <- p+facet_wrap(~Variable,scales='free_y')
+    }
+    p <- p+theme_bw()+
+      theme(legend.position='right', strip.background=element_blank(),panel.grid.minor=element_blank(),
+            axis.text.x=element_text(angle=45, hjust=1))
+    if(name_file!=F){
+      ggsave(filename=paste0(odir,"/general/",name_file,".png"), plot=p, width=w, height=h, units='mm', dpi=300)
+    }
+    return(p)
+  } else {
+    print('error: empty data frame')
+  }
+}
+
+fplot_scatter <- function(vars, xlab=NULL, ylab=NULL, w=150, h=100, cpt=NULL, name_file){
+  dt <- filter(df_all, Variable%in%vars) %>%
+    spread(key=Variable, value=Value, fill=NA) %>%
+    rename(Value_x=vars[1], Value_y=vars[2]) %>%
+    mutate(Scenario=factor(dt$Scenario, levels=scen_mat$Scenario), Year=as.character(Year)) %>%
+    filter(Scenario!='historical')
+  if(nrow(dt)>0){
+    p <- ggplot(data=dt)+
+      geom_point(aes(x=Value_x, y=Value_y, color=Year, shape=Scenario), show.legend=T, alpha=1)+
+      labs(title=cpt, x=xlab, y=ylab)+
+      theme_bw()+
+      theme(legend.position='right', strip.background=element_blank(),panel.grid.minor=element_blank())
+    if(name_file!=F){
+      ggsave(filename=paste0(odir,"/plot/",reg_in,"/", name_file, ".png"), plot=p, width=w, height=h, units='mm', dpi=300)
+    }
+    return(p)
+  } else {
+    print('error: empty data frame')
+  }
+}
+
+fplot_loadcurve <- function(dt_inp,reg,scen, ylabel, w=150, h=100, cpt=NULL, name_file){
+  dt <- filter(dt_inp, Region %in% reg, Scenario %in% scen) %>%
+    mutate(Year=as.character(Year))
+  if(nrow(dt)>0){
+    p <- ggplot(data=dt)+
+      geom_path(aes(x=Time, y=Value, group=Year, color=Year), show.legend=T, alpha=1)+
+      facet_grid(Season~Day)+
+      labs(title=cpt, x=NULL, y=ylabel)+
+      theme_bw()+
+      theme(legend.position='right', strip.background=element_blank(),panel.grid.minor=element_blank())
+    if(name_file!=F){
+      ggsave(filename=paste0(odir,"/plot/",reg_in,"/", name_file, ".png"), plot=p, width=w, height=h, units='mm', dpi=300)
+    }
+    return(p)
+  } else {
+    print('error: empty data frame')
+  }
+}
---
+++ prog/index.R
@@ -0,0 +1,58 @@
+# Make output directory ---------------------------------------------------
+
+if(!dir.exists(paste(odir,"index",sep="/"))){dir.create(paste(odir,"index",sep="/"),recursive=T)}
+pdf(paste(odir,'index','plot_main.pdf',sep="/"),width=10,height=7)
+
+# Function ----------------------------------------------------------------
+
+fplot_share <- function(var,totvar,ylabel,ylim=c(0,NA),ylab=waiver(),name_file,cpt=NULL){
+  varlist <- str_c(totvar,var,sep="_")
+  dt <-  df_all %>% 
+    filter(Variable %in% c(totvar,varlist)) %>% 
+    pivot_wider(names_from = Variable, values_from = Value) %>%
+    mutate(across(c(starts_with(totvar)),~.x*100/eval(parse(text=totvar)))) %>% 
+    select(-eval(totvar)) %>% 
+    pivot_longer(cols=c(starts_with(totvar)),names_to="Variable",values_to="Value") %>% 
+    mutate(Scenario=factor(Scenario, levels=scen_mat$Scenario)) %>%
+    filter(Scenario!='historical')
+  w <- 250
+  h <- ceiling(length(varlist)/3)*75
+  if(nrow(dt)>0){
+    p <- ggplot(data=dt)+
+      geom_path(aes(x=Year, y=Value, color=Scenario), show.legend=T, alpha=1)+
+      labs(title=cpt, x=NULL, y=ylabel)+
+      scale_y_continuous(limits=ylim, labels=ylab)
+    if(length(var) > 1){
+      p <- p+facet_wrap(~Variable,scales='free_y',ncol=3)
+    }
+    p <- p+theme_bw()+
+      theme(legend.position='right', strip.background=element_blank(),panel.grid.minor=element_blank(),
+            axis.text.x=element_text(angle=45, hjust=1))
+    if(name_file!=F){
+      ggsave(filename=paste0(odir,"/index/",name_file,".png"), plot=p, width=w, height=h, units='mm', dpi=300)
+    }
+    return(p)
+  } else {
+    print('error: empty data frame')
+  }
+}
+
+
+# Plot --------------------------------------------------------------------
+
+var_prm <- c("Oil","Coa","Gas","Solar","Win")
+fplot_share(var_prm,"Prm_Ene",name_file="SHR_prm",ylabel="Share in primary energy (%)",cpt="SHR_prm")
+
+var_fin <- c("Liq_Oil","SolidsCoa","Gas","Ele","Hyd")
+fplot_share(var_fin,"Fin_Ene",name_file="SHR_fin",ylabel="Share in final energy (%)",cpt="SHR_fin")
+
+var_finind <- c("Liq_Oil","SolidsCoa","Gas","Ele","Hyd")
+fplot_share(var_finind,"Fin_Ene_Ind",name_file="SHR_finind",ylabel="Share in final energy (%)",cpt="SHR_finind")
+
+var_finbui <- c("Liq_Oil","SolidsCoa","Gas","Ele","Hyd")
+fplot_share(var_finbui,"Fin_Ene_Res_and_Com",name_file="SHR_finbui",ylabel="Share in final energy (%)",cpt="SHR_finbui")
+
+var_fintra <- c("Liq_Oil","SolidsCoa","Gas","Ele","Hyd")
+fplot_share(var_fintra,"Fin_Ene_Tra",name_file="SHR_fintra",ylabel="Share in final energy (%)",cpt="SHR_fintra")
+
+dev.off()
